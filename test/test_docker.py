2834a5cbc6ece8eb6968f0881d68d900862703a7
Kevin <kevin.jablonka@epfl.ch>
2020-09-22 11:28:18 +0200

    chore: added test of the API

---
+++ test/test_docker.py
@@ -0,0 +1,15 @@
+import requests
+import sys
+
+with open("xalxuf01.cif", "r") as fh:
+    f = fh.read()
+
+
+r = requests.post("http://localhost:8091/api/predictxrd/", data={"structurefile": f})
+keys = r.json().keys()
+
+if "x" in keys:
+    print('PXRD prediction API works')
+    sys.exit(0)
+else:
+    sys.exit(1)
