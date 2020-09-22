d78cde5f5a7a7c92c71a7d5338c70967c232914f
Kevin <kevin.jablonka@epfl.ch>
2020-09-22 10:02:05 +0200

    chore: adding CI for flask app #4

--- test/test_api.py
+++ test/test_api.py
@@ -1,16 +1,17 @@
-from ... import app
-import json 
+import sys
+
+sys.path.append("..")
+from app import app
+import json
 
 with open("xalxuf01.cif", "r") as fh:
     f = fh.read()
 
 
 def test_pxrd():
-    response = app.test_client().post(
-        "/api/predictxrd", structurefile=f, content_type="application/json",
-    )
-
+    response = app.test_client().post("/api/predictxrd/", data=dict(structurefile=f))
     data = json.loads(response.get_data(as_text=True))
-
+    keys = data.keys()
     assert response.status_code == 200
-
+    assert "x" in keys
+    assert "y" in keys
