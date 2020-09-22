0a987d8235308e97afda3a20c748c6486ed4e2c2
Kevin <kevin.jablonka@epfl.ch>
2020-09-22 09:06:46 +0200

    chore: prediction implementing, now adding tests

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,11 @@ WORKDIR /home/lsmoler
 
 COPY requirements.txt .
 
+COPY app.py . 
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 USER lsmoler
 
-CMD gunicorn -b 0.0.0.0:$PORT -c gunicorn_conf.py --log-level debug --capture-output --enable-stdio-inheritance run_app:server
+CMD gunicorn -b 0.0.0.0:$PORT -c gunicorn_conf.py --log-level debug --capture-output --enable-stdio-inheritance app:app
 
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, make_response, request, abort
 from pymatgen import Structure
+from pymatgen.analysis.diffraction.xrd import XRDCalculator
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ def index():
     return "We predict PXRD patterns"
 
 
+# Todo: potentially also allow to request different light sources
 @app.route("/api/predict/", methods=["POST"])
 def index(cif):
     if "structurefile" not in request.files:
@@ -20,7 +22,19 @@ def index(cif):
     try:
         s = Structure.from_str(filecontent)
     except Exception:
-        abort(500)
+        abort(512)
+
+    try:
+        calculator = XRDCalculator()
+        pattern = calculator.get_pattern(s)
+        x = list(pattern.x)
+        y = list(pattern.y)
+        hkls = pattern.hkls
+
+        return jsonify({"x": x, "y": y, "hkls": hkls,}), 200
+        
+    except Exception:
+        abort(513)
 
 
 @app.errorhandler(404)
@@ -28,8 +42,15 @@ def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
-@app.errorhandler(501)
-def not_found(error):
+@app.errorhandler(513)
+def could_not_calculate(error):
+    return make_response(
+        jsonify({"error": "Calculation of the XRD pattern failed"}), 502
+    )
+
+
+@app.errorhandler(512)
+def could_not_read(error):
     return make_response(jsonify({"error": "Could not read structure file"}), 501)
 
 
