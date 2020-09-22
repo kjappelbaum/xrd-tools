7065f100d85e3a84598b7c4d2196ec1dc6757be9
Kevin <kevin.jablonka@epfl.ch>
2020-09-22 07:52:40 +0200

    chore: raise error when we cannot read the structure

--- app.py
+++ app.py
@@ -13,14 +13,24 @@ def index():
 def index(cif):
     if "structurefile" not in request.files:
         abort(400)
-    structurefile = request.files["structurefile"]
 
+    structurefile = request.files["structurefile"]
     filecontent = structurefile.read().decode("utf-8")
+
+    try:
+        s = Structure.from_str(filecontent)
+    except Exception:
+        abort(500)
 
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
+
+
+@app.errorhandler(501)
+def not_found(error):
+    return make_response(jsonify({"error": "Could not read structure file"}), 501)
 
 
 if __name__ == "__main__":
