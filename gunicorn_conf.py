2d41f33c8c11c3f2851bd38913a8d3e8e9f4b68f
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-10-14 17:05:29 +0200

    chore: cleanup

--- gunicorn_conf.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# pylint:disable=invalid-name
-"""Settings for gunicorn"""
-
-#https://pythonspeed.com/articles/gunicorn-in-docker/
-workers = 2
-threads = 4
-worker_class = 'gthread'
